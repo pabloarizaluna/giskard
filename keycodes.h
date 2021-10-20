6a2dea2c27addf577819e4b212d789eecb22b6b4
Pablo Ariza Luna <pabloarizaluna@users.noreply.github.com>
2021-10-19 22:08:24 -0500

    Release (#2)
    * RGB implemented (#1)
    
    * Lighting effects added, needs some tuning
    
    * Rgb fixed and code cleaned
    
    * hotfix: tuning code and fixing the layer code

---
+++ keycodes.h
@@ -0,0 +1,16 @@
+#ifndef _KEYCODES_H_
+#define _KEYCODES_H_
+
+enum layers {
+  _DVORAK,
+  _SYMBOLS,
+  _NUMBERS,
+  _ADJUST,
+};
+
+#define L_DVORAK 0
+#define L_SYMBOLS 2
+#define L_NUMBERS 4
+#define L_ADJUST 8
+
+#endif
