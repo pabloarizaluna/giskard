0ffc545bba3bd96546b10e5a602180cf668a1993
Pablo Ariza <pablo.arizal@pm.me>
2022-08-22 08:02:31 -0500

    Updating keymap to keep up

--- config.h
+++ config.h
@@ -47,6 +47,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGB_MATRIX_VAL_STEP 8
 #    define RGB_MATRIX_SPD_STEP 10
 #    define RGB_MATRIX_STARTUP_SPD 90
+#    define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
 
 /* Disable the animations you don't want/need.  You will need to disable a good number of these    *
  * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
